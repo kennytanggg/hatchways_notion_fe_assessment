eadd300e46a752256cd35ddbfac59ccac479203c
Kenny Tanglao <ktanglao@gmail.com>
2022-09-05 10:57:40 -0400

    JS cleanup, ToDo: build using React

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
-// API key - Openweathermap - b7dc88a8227d0789516251f9056a5c12
-//http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={API key}
-// Icon is based on API response (icon)
-
 const api_key = 'b7dc88a8227d0789516251f9056a5c12';
-const url = `http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=${api_key}`;
-
-const zip_code = 11220;
-const country_code = 'US';
-// const url2 = `http://api.openweathermap.org/geo/1.0/zip?zip=E14,GB&appid=${api_key}`;
-const url2 = `http://api.openweathermap.org/geo/1.0/zip?zip=${zip_code},${country_code}&appid=${api_key}`;
-
+// const url = `http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=${api_key}`;
+// const zip_code = 11220;
+// const country_code = 'US';
+// const url2 = `http://api.openweathermap.org/geo/1.0/zip?zip=${zip_code},${country_code}&appid=${api_key}`;
 // Open Weather API Response Geocoding
 /*
 {zip: '11228', name: 'New York', lat: 40.6174, lon: -74.0121, country: 'US'}
@@ -32,21 +25,7 @@ const lat = 40.6174;
 const lon = -74.0121;
 const url_5dayforecast = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${api_key}&units=imperial`;
 
-// Get data from API
-// Create class for App
-// Create class UI
-
-// 08-27-2022
-// Plan for today
-// Figure out how to use API to get weather forecast for today
-// Get the data for 5 days
-// Map the data to the UI
-
 class App {
-	// constructor() {
-	// 	this.getWeatherData();
-	// }
-	// KT: Could make these static methods
 	async getWeatherData() {
 		const response = await fetch(url_5dayforecast);
 		if (response.ok) {
@@ -57,28 +36,14 @@ class App {
 	}
 
 	get5DayForecast(data) {
-		// Return today's high and low temperatures, and the next 4 days
+		// Return today's high and low temperatures over the next 4 days
 		// Map data to UI (day, temp_high, temp_low, description, img)
 		data.list.forEach((time) => {
 			if (time.dt_txt.includes('12:00:00')) {
-				//#region
-				/*
-			<div class="day-container">
-				<label>Wednesday</label>
-				<img src="http://openweathermap.org/img/wn/01d@2x.png"></img>
-				<div class='temperature-container'>
-					<label class='temperature-high'>95</label>
-					<label class='temperature-low'>88</label>
-				</div>
-			</div>
-			*/
-				//#endregion
 				const day_container = document.createElement('div');
 				day_container.className = 'day-container';
 				const day_label = document.createElement('label');
 				day_label.innerText = DateUtil.convertDayToText(new Date(time.dt_txt).getDay());
-
-				console.log(time);
 
 				let {
 					main: { temp_max: high_temp, temp_min: low_temp },
@@ -86,7 +51,6 @@ class App {
 						0: { icon: icon }, //Gets the primary weather condition
 					},
 				} = time;
-				console.log(low_temp);
 
 				const img = document.createElement('img');
 				img.src = `http://openweathermap.org/img/wn/${icon}@2x.png`;
@@ -105,26 +69,9 @@ class App {
 				temperature_container.append(high_temp_label, low_temp_label);
 				day_container.append(day_label, img, temperature_container);
 
-				console.log(day_container, temperature_container);
-
 				document.querySelector('.weather-container').append(day_container);
 			}
 		});
-
-		// Create an empty array with weather objects
-		// Iterate through each one, map the data
-
-		// Day 1 - tomorrow's forecast at noon
-		// Day 2 - the next day's forecast at noon
-		// Day 3 - the next day's forecast at noon
-		// Day 4 - the next day's forecast at noon
-		// Day 5 - the next day's forecast at noon
-
-		// console.log(weatherdata, initial_result_date);
-
-		// if (date != !isTodaysDate) {
-		// 	console.log('false');
-		// }
 	}
 
 	initialize() {
